d653c2bded048004243dcd657714b75dc5137fe3
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2024-07-14 17:47:47 +0200

    🐛 FIX: Arranger problème windows déploement

--- urban-roots/app/carte/page.jsx
+++ urban-roots/app/carte/page.jsx
@@ -1,14 +1,20 @@
 "use client";
 
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+
+// Importer dynamiquement les composants react-leaflet avec SSR désactivé
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
 // Fix for default marker icons not showing
 if (typeof window !== 'undefined') {
+  const L = require('leaflet');
   delete L.Icon.Default.prototype._getIconUrl;
   L.Icon.Default.mergeOptions({
     iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
@@ -27,8 +33,8 @@ const fetchData = async () => {
 };
 
 const Carte = () => {
-
   const [markers, setMarkers] = useState([]);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     fetchData().then(data => setMarkers(data)).catch(error => console.error('Error fetching markers:', error));
@@ -43,39 +49,41 @@ const Carte = () => {
     }
   }, []);
 
+  useEffect(() => {
+    // Set the isClient state to true when the component is mounted on the client
+    setIsClient(true);
+  }, []);
+
   return (
-
     <>
-
       <Navbar />
       
       <div className="bg-white text-gray-800">
-        <MapContainer center={[48.819555930925, 2.2994846425247]} zoom={7} style={{ height: '100vh', width: '70%' }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {markers.map((marker, index) => (
-            <Marker key={index} position={[marker.lat, marker.lng]}>
-              <Popup>
-                <div>
-                  <h2 className="font-bold">{marker.title}</h2>
-                  <p><strong>Type de Projet:</strong> {marker.list_typeprojet.join(', ')}</p>
-                  <p><strong>Code Postal:</strong> {marker.cp}</p>
-                  <p><strong>Ville:</strong> {marker.ville}</p>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+        {isClient && (
+          <MapContainer center={[48.819555930925, 2.2994846425247]} zoom={7} style={{ height: '100vh', width: '70%' }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {markers.map((marker, index) => (
+              <Marker key={index} position={[marker.lat, marker.lng]}>
+                <Popup>
+                  <div>
+                    <h2 className="font-bold">{marker.title}</h2>
+                    <p><strong>Type de Projet:</strong> {marker.list_typeprojet.join(', ')}</p>
+                    <p><strong>Code Postal:</strong> {marker.cp}</p>
+                    <p><strong>Ville:</strong> {marker.ville}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        )}
       </div>
 
       <Footer />
-    
     </>
-    
-    
-  )
-}
+  );
+};
 
-export default Carte
+export default Carte;
