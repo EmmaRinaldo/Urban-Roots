48fda2d8df0511faa0d65bd98efaacfe6e3f0b59
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2024-07-14 17:42:31 +0200

    🐛 FIX: Arranger erreur déploiement

--- urban-roots/app/carte/page.jsx
+++ urban-roots/app/carte/page.jsx
@@ -8,12 +8,14 @@ import L from 'leaflet';
 import { useEffect, useState } from 'react';
 
 // Fix for default marker icons not showing
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-});
+if (typeof window !== 'undefined') {
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  });
+}
 
 const fetchData = async () => {
   const response = await fetch('/api/markers');
@@ -29,14 +31,16 @@ const Carte = () => {
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
-    fetchData().then(data => setMarkers(data)).catch(error => console.error(error));
+    fetchData().then(data => setMarkers(data)).catch(error => console.error('Error fetching markers:', error));
   }, []);
 
   useEffect(() => {
     // This useEffect is used to adjust the map size after rendering
-    setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 500);
+    if (typeof window !== 'undefined') {
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'));
+      }, 500);
+    }
   }, []);
 
   return (
