1b5bb7906a00a382432b75fff910203d847d59ee
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2024-07-23 10:05:00 +0200

    🐛 FIX: Bug Redirection Page Login

--- urban-roots/app/api/auth/creation/route.ts
+++ urban-roots/app/api/auth/creation/route.ts
@@ -37,5 +37,5 @@ export async function GET(req: NextRequest) {
         throw new Error("CREATION_REDIRECT_URL is not defined");
     }
     
-    return NextResponse.redirect(redirectUrl);
+    return NextResponse.redirect(process.env.NODE_ENV ==='development' ? "https://localhost:3000/" : "https://urbanroots.fr/");
 }
