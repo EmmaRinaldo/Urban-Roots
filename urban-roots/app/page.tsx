dce96a455b01e231e7f1747dba36bb2e64b81df7
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2024-10-18 21:25:18 +0200

    🐛 FIX: Bug de déploiement

--- urban-roots/app/page.tsx
+++ urban-roots/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { HeroSection } from "./components/HeroSection";
 import { About } from "./components/About";
 import { SectionCarte } from "./components/SectionCarte";
-import { SectionRessources } from "./components/SectionRessources";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
